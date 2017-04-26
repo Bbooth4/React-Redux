78798dabcff1a0547bae5510bad30efd2acadd9c
Brandon <booth.brandon4@gmail.com>
2017-04-26 14:38:28 -0400

    first click hanlders

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,44 @@
 import React, { PropTypes } from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: ""}
+    };
+
+    // this.onTitleChange = this.onTitleChange.bind(this);
+    // this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange = (e) => {
+    console.log('onTitleChange')
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({ course: course });
+  }
+
+  onClickSave =(e) => {
+    console.log('onClickSave')
+    // alert(`words`);
+  }
+
   render() {
     return (
       <div className="jumbotron">
         <h1>Courses </h1>
+        <h2>Add Course</h2>
+        <input 
+          type="text"
+          value={this.state.course.title}
+          onChange={this.onTitleChange}
+        />
+        <input 
+          type="submit"
+          onClick={this.onClickSave}
+          value="Save"
+        />
       </div>
     );
   }
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -9,7 +9,7 @@ class HomePage extends React.Component {
         <p>React, Redux and React Router in ES6 for ultra-responsibe web apps.</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
       </div>
-    )
+    );
   }
 }
 
